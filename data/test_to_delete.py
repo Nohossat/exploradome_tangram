ae2b6b44b0f274504e7e70fff44bd7ea48662115
Nohossat TRAORE <nohossat.traore@gmail.com>
2020-07-28 15:37:01 +0200

    add hu_moments + moments values

--- data/test_to_delete.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import pandas as pd
-# import cv2
-
-df = pd.read_csv("moments.csv")
-print(df)
--- moments.py
+++ moments.py
@@ -19,41 +19,7 @@ def get_files():
             images[filename] = os.path.join(dirname, file)
     return images
 
-def preprocess_img(img,left_side=True):
-    '''
-    this function takes a cv image as input, calls the resize function then crops the image to keep only the board.
-    It then chooses the left or right half of the board, based on user input 
-    '''
-
-    img =resize(img,20)[0:-50, 55:-100].copy() # reisze img to 20% and crop to keep only board
-    if left_side == True:
-        img = img[0:int(img.shape[0]),0:int(img.shape[1]/2)] # keep only the left half of the board
-    elif left_side == False:
-        img = img[0:int(img.shape[0]),int(img.shape[1]/2):] # keep only the right half of the board
-    return img
-
-def find_humos(img,filename, sensitivity_to_light=50):
-<<<<<<< HEAD
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # binarize img
-=======
-    '''
-    this function finds the largest dark shape in the image and returns the shape's Hu Moments.
-    '''
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #binarize img
->>>>>>> ffa73666f2c9f77e04c4186e1e5f15871bf74f85
-    gray[gray>sensitivity_to_light] = 0 # turn background to black
-    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)[1]
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-    lst_moments = [cv2.moments(c) for c in cnts] # retrieve moments of all shapes identified
-    lst_areas = [i["m00"] for i in lst_moments] # retrieve areas of all shapes
-    
-    max_idx = lst_areas.index(max(lst_areas)) # select shape with the largest area
-    HuMo = cv2.HuMoments(lst_moments[max_idx]) # grab humoments for largest shape
-    HuMo = np.append(HuMo, filename)
-    return HuMo
-
-def resize(img,percent=20):
+def resize(img, percent=20):
     '''
     this function takes a cv image as input and resizes it. The primary objective is to make the contouring less sensitive to between-tangram demarcation lines,
     the secondary objective is to speed up processing.
@@ -65,15 +31,67 @@ def resize(img,percent=20):
     img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA).copy()
     return img
 
+def preprocess_img(img, left_side=True, sensitivity_to_light=50):
+    '''
+    this function takes a cv image as input, calls the resize function, crops the image to keep only the board, 
+    chooses the left or right half of the board, based on user input and eventually finds the largest dark shape
+    '''
+
+    # resize operations
+    img = resize(img,20)[0:-50, 55:-100].copy() # resize img to 20% and crop to keep only board
+    # img = resize(img, 20).copy() # just resize
+
+    if left_side:
+        img = img[0:int(img.shape[0]),0:int(img.shape[1]/2)] # keep only the left half of the board
+    else:
+        img = img[0:int(img.shape[0]),int(img.shape[1]/2):] # keep only the right half of the board
+    
+    # get the largest shape
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # binarize img
+    gray[gray>sensitivity_to_light] = 0 # turn background to black
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY)[1] # threshold ???
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    return cnts, img
+
+def find_moments(cnts, filename=None, hu_moment = True):
+    '''
+    this function returns the shape's Hu Moments.
+    '''
+    lst_moments = [cv2.moments(c) for c in cnts] # retrieve moments of all shapes identified
+    lst_areas = [i["m00"] for i in lst_moments] # retrieve areas of all shapes
+    
+    max_idx = lst_areas.index(max(lst_areas)) # select shape with the largest area
+
+    if hu_moment: # if we want the Hu moments instead
+        HuMo = cv2.HuMoments(lst_moments[max_idx]) # grab humoments for largest shape
+        if filename:
+            HuMo = np.append(HuMo, filename)
+        return HuMo
+
+    # if we want to get the moments
+    Moms = lst_moments[max_idx] 
+    if filename:
+        Moms['target'] = filename
+    return Moms
+
+
 def save_moments(images):
     """
     compute moments for all images in our dataset
     """
     hu_moments = []
+    moments = []
+
     for image_name, image_path in images.items():
-        image = cv2.imread(image_path)
-        hu_moments.append(find_humos(image, image_name))
-    return np.array(hu_moments).reshape(12, 8)
+        img_cv = cv2.imread(image_path)
+        cnts, img = preprocess_img(img_cv)
+        display_contour(cnts, img)
+        hu_moments.append(find_moments(cnts, image_name))
+        moments.append(find_moments(cnts, image_name, hu_moment=False))
+
+    return hu_moments, moments
 
 def compare_moments_with_labels():
     """
@@ -92,11 +110,12 @@ def read_video():
         ret, image = cap.read()
 
         # Our operations on the frame come here
-        cnts = img_preprocessing(image)
-        moments = get_moment(image)
+        cnts_right, img = preprocess_img(image, left_side=False)
+        # img_false = preprocess_img(image)
+
+        HuMo = find_moments(cnts_right)
 
         proba = compare_moments_with_labels()
-
         print(proba)
 
         # Display the resulting frame
@@ -107,12 +126,25 @@ def read_video():
     cap.release()
     cv2.destroyAllWindows()
 
-
+def display_contour(cnts, img):
+    for c in cnts:
+        #compute the center of the contour
+        M = cv2.moments(c)
+        huM = cv2.HuMoments(M)
+        # draw the contour and center of the shape on the image
+        cv2.drawContours(img, [c], -1, (0, 255, 0), 2)
+    # show the image
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
 
 if __name__ == '__main__':
     # if we call this file directly, we compute the moments for all the classes
     # save moments for each class
     images = get_files()
-    hu_moments = save_moments(images)
+
+    hu_moments, moments = save_moments(images)
     hu_moments_df = pd.DataFrame(hu_moments)
-    hu_moments_df.to_csv('hu_moments.csv', index=False)
+    hu_moments_df.to_csv('data/hu_moments.csv', index=False)
+
+    moments_df = pd.DataFrame(moments)
+    moments_df.to_csv('data/moments.csv', index=False)
