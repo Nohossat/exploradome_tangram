92e444ccc67998378564f365294b5e670e7177bb
Nohossat TRAORE <nohossat.traore@gmail.com>
2020-07-30 18:07:20 +0200

    fix import issue in distances_test.py file

--- tests/distances_test.py
+++ tests/distances_test.py
@@ -5,17 +5,17 @@ Created on Tue Jul 28 15:11:06 2020
 @author: Renata
 """
 import pytest
-import distances.py
+from exploradome_tangram.distances import *
 
 #test of distance humoment1
-def dist_humoment1_test():
-    assert dist_humoment1(0.026322267294741648, 0.0027161374010481088) == 330.179238664131
+def test_dist_humoment1():
+    assert dist_humoment1(0.026322267294741648, 0.0027161374010481088) == 330.17923866413116, "result for dist_humoment1 not ok"
 
 #test of distance humoment2
-def dist_humoment2_test():
-    assert dist_humoment2(0.026322267294741648, 0.0027161374010481088) == 0.0236061298936935
+def test_dist_humoment2():
+    assert dist_humoment2(0.026322267294741648, 0.0027161374010481088) == 0.02360612989369354, "result for dist_humoment2 not ok"
 
 #test of distance humoment3
-def dist_humoment3_test():
-    assert dist_humoment3(0.026322267294741648, 0.0027161374010481088) == 8.69106617529156
-    #message erreur, commentaire
+def test_dist_humoment3():
+    assert dist_humoment3(0.026322267294741648, 0.0027161374010481088) == 8.691066175291558, "result for dist_humoment3 not ok"
+    #message erreur, commentaire 
