0f71544a4d2104dfb8bab13aaac7f30fabd7ddd9
renatakaczor <renatakaczor@sfr.fr>
2020-08-04 12:26:19 +0200

    test app.py

--- tests/app_test.py
+++ tests/app_test.py
@@ -8,7 +8,7 @@ from ..app import *
 def test_tangram_game():
    # test the probabilities of the image / frame 
    path = "../data/tangrams/cygne.jpg"
-   probality = tangram_game(crop=False,image=path)
-   assert isinstance(probalitity, pd.core.frame.DataFrame), 'Predictions should be dataframe'
-   assert probalitity.loc[0, 'target'] == 'cygne', 'Predictions should be bateau'
+   probality = tangram_game(hu_moments_dataset='../data/hu_moments.csv', crop=False,image=path)
+   assert isinstance(probality, pd.core.frame.DataFrame), 'Predictions should be dataframe'
+   assert probality.loc[0, 'target'] == 'cygne', 'Predictions should be bateau'
    
